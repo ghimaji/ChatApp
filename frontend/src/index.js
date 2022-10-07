2eb7dafb5e7cbf199ae5d03a423488379e4a7e2f
intmaster <ghimajiriadh@gmail.Com>
2022-10-07 23:42:33 +0100

    using react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,12 @@
-console.log("hallo");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chat from './containers/Chat';
+class App extends React.Component {
+    render(){
+
+        return (
+            <Chat />
+    )
+    }
+}
+ReactDOM.render(<App />,document.getElementById('app'));
